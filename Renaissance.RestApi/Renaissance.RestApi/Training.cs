50d96f2aa25ce75484c1789390c9e0242a914657
Jeffrey Valeroso <jfvaleroso.smart@gmail.com>
2014-01-14 01:07:14 +0800

    Initial upload

---
+++ Renaissance.RestApi/Renaissance.RestApi/Training.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.XPath;
+using umbraco.cms.businesslogic.web;
+
+namespace Renaissance.RestApi
+{
+    public class Training
+    {
+
+        public static string HelloWorld()
+        {
+            return "Hi";
+        }
+
+        public static string Hello(string name)
+        {
+            return "Hi" + name;
+        }
+
+        public static XPathNodeIterator  GetDataById(int id)
+        {
+          return umbraco.library.GetXmlNodeById(id.ToString());
+        }
+
+        public static string Create(int parentId, string name)
+        {
+            DocumentType dt = DocumentType.GetByAlias("Textpage");
+            Document d = Document.MakeNew(name, dt, new umbraco.BusinessLogic.User(0), parentId);
+            d.Publish(new umbraco.BusinessLogic.User(0));
+            d.Save();
+
+            umbraco.library.PublishSingleNode(d.Id);
+            return umbraco.library.NiceUrl(d.Id);
+        }
+
+
+
+
+    }
+}
---
+++ Renaissance.RestApi/Renaissance.RestApi/readme.txt
@@ -0,0 +1,23 @@
+﻿open restextension.config
+
+<?xml version="1.0" encoding="utf-8"?>
+<RestExtensions>
+  <!--
+	<ext assembly="umbraco" type="umbraco.presentation.umbracobase.library.member" alias="currentMember">
+    <permission method="login" allowAll="true" />
+    <permission method="logout" allowAll="true" />
+    <permission method="id" allowAll="true" />
+    <permission method="data" allowAll="true" />
+    <permission method="logout" allowAll="true" />
+    <permission method="setProperty" allowAll="false" />
+  </ext>
+-->
+
+  <ext assembly="Renaissance.RestApi" type="Renaissance.RestApi.Training" alias="training">
+    <permission method="HelloWorld" allowAll="true" />
+    <permission method="Hello" allowAll="true" />
+    <permission method="GetDataById" allowAll="true" />
+    <permission method="Create" allowAll="true" />
+  </ext>
+  
+</RestExtensions>
